cdb449f84759eb87bc4d1d951d0fff2a5339966f
Jeffrey Steven <jeffreysteven77@gmail.com>
2018-10-15 17:56:29 +0200

    Second

--- app/src/main/java/com/example/jeffrey/practiceapp/MainActivity.java
+++ app/src/main/java/com/example/jeffrey/practiceapp/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.jeffrey.practiceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mFirebaseButton;
+    private DatabaseReference mDatabase;
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -15,10 +22,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseButton = (Button) findViewById(R.id.button1);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        mFirebaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mDatabase.child("Name").setValue("Jeff");
+            }
+        });
+
     }
 
     /**
